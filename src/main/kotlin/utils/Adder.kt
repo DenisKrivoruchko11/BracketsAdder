f5d19d029c8bcbb161779eb818a7f33cf8e4d27a
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-16 06:11:58 +0100

    add solution without tests

---
+++ src/main/kotlin/utils/Adder.kt
@@ -0,0 +1,38 @@
+package utils
+
+class Adder(private val input: String) {
+
+    fun add(addToMiddle: Boolean, placeAroundEdges: Boolean, brackets: List<Pair<Char, Char>>): String {
+        val firstHalfBuilder = StringBuilder()
+        val secondHalfBuilder = StringBuilder()
+
+        var bracketsNumber = 0
+
+        if (placeAroundEdges) {
+            firstHalfBuilder.append(brackets[bracketsNumber % brackets.size].first)
+            secondHalfBuilder.append(brackets[bracketsNumber % brackets.size].second)
+
+            bracketsNumber++
+        }
+
+        val steps = input.length / 2 + input.length % 2
+
+        for (i in 0 until steps) {
+            if (i < steps - 1 || addToMiddle) {
+                firstHalfBuilder
+                    .append("${input[i]}${brackets[bracketsNumber % brackets.size].first}")
+                
+                secondHalfBuilder
+                    .append("${input[input.lastIndex - i]}${brackets[bracketsNumber % brackets.size].second}")
+
+                bracketsNumber++
+            } else {
+                firstHalfBuilder.append("${input[i]}")
+
+                if (i != input.lastIndex - i) secondHalfBuilder.append("${input[input.lastIndex - i]}")
+            }
+        }
+
+        return firstHalfBuilder.toString() + secondHalfBuilder.toString().reversed()
+    }
+}
